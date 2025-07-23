f9716da3dff62ab608ff61f340cad9fa3103980f
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-07-23 19:47:56 +0545

    initial commit with add navbar section and mobil navbar section

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import NavbarSection from './components/navbarSection'
+
+export default function App() {
+  return (
+    <div className='text-3xl font-bold'>
+     <NavbarSection/>
+    </div>
+  )
+}
